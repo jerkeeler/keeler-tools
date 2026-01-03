236070746bd9cd8404bd572fdc63139821eb7055
Jeremy Keeler <jeremy@keeler.dev>
2026-01-03 13:35:37 -0500

    Remove Google Fonts preload and use system font stack

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -10,8 +10,8 @@ export default {
                 },
             },
             fontFamily: {
-                sans: ['IBM Plex Sans', 'system-ui', 'sans-serif'],
-                display: ['Space Grotesk', 'system-ui', 'sans-serif'],
+                sans: ['system-ui', 'sans-serif'],
+                display: ['system-ui', 'sans-serif'],
             },
         },
     },
